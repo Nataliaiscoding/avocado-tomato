58d49c981dcafc2b814c06db106f84842cc64898
Nataliaiscoding <119931845+Nataliaiscoding@users.noreply.github.com>
2024-10-10 21:45:07 +0200

    Update Avocado.js

--- src/Avocado.js
+++ src/Avocado.js
@@ -1,11 +1,9 @@
-
-
+import avocado from './avocado.jpeg';
 function Avocado() {
     return (
         <div>
-            <img src="https://images.unsplash.com/photo-1523049673857-eb18f1d7b578?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="avocado" />
+            <img src={avocado} alt="avocado" />
         </div>
     )
 }
-
 export default Avocado;
