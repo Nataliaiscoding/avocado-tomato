61084bb1c045cea57742ebca3ce800ecaa186dd1
Nataliaiscoding <119931845+Nataliaiscoding@users.noreply.github.com>
2024-10-10 21:42:12 +0200

    Update Avocado.js

--- src/Avocado.js
+++ src/Avocado.js
@@ -3,7 +3,7 @@
 function Avocado() {
     return (
         <div>
-            <img src="https://github.com/Nataliaiscoding/avocado-tomato/blob/main/src/avocado.jpeg?raw=true" alt="avocado" />
+            <img src="https://images.unsplash.com/photo-1523049673857-eb18f1d7b578?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="avocado" />
         </div>
     )
 }
